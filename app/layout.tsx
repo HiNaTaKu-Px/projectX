ecee93db668f322257416855134951c17c4d712a
hikaru3183-sketch <hikaru3183@gmail.com>
2026-02-17 00:15:53 +0900

    認証系をluciaに変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,22 @@
-"use client";
-
 import "./globals.css";
-import { usePathname, useRouter } from "next/navigation";
+import Header from "@/components/layout/Header";
 
-export default function RootLayout({
-  children,
-}: {
+export const metadata = {
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+type LayoutProps = {
   children: React.ReactNode;
-}) {
-  const pathname = usePathname();
-  const router = useRouter();
+};
 
-  const isHome = pathname === "/";
-  const hideHeader = isHome;
-
+export default function RootLayout({ children }: LayoutProps) {
   return (
     <html lang="ja">
       <body className="m-0 p-0 overflow-auto bg-gray-700">
-        {/* ホーム以外はヘッダー常時表示 */}
-        {!hideHeader && (
-          <div className="fixed top-0 left-0 right-0 z-50">
-            <header className="w-full bg-gray-700 text-white p-0  shadow-lg flex items-center">
-              <button
-                onClick={() => router.push("/")}
-                className="text-lg font-bold hover:opacity-70 transition"
-              >
-                ← ホーム
-              </button>
-            </header>
-          </div>
-        )}
-
-        {/* ★ 余白なしで子ページをそのまま表示 */}
-        <div className="">{children}</div>
+        <Header />
+        {children}
       </body>
     </html>
   );
